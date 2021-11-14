631eac1f636a454d3a42932d604fa399245cf512
李嘉博 <1658446745@qq.com>
2021-11-14 15:51:52 +0800

    完善了画圆算法

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,6 +11,9 @@ MainWindow::MainWindow(QWidget *parent)
     connect(ui->actionDrawRect,&QAction::triggered,this,[=](){//绘制矩形
         this->state=RECT;
     });
+    connect(ui->actionDrawCircle,&QAction::triggered,this,[=](){
+        this->state=CIRCLE;
+    });
 }
 
 MainWindow::~MainWindow()
@@ -39,6 +42,8 @@ void MainWindow::paintEvent(QPaintEvent *)
         }
         break;
     case CIRCLE:
+        for(Circle c:circles)
+            BresenhamCircle(&painter,c.x,c.y,(int)c.r);
         break;
     case POLYGON:
         break;
@@ -62,6 +67,9 @@ void MainWindow::mousePressEvent(QMouseEvent *event)
         rects.push_back(QRect(rectX1,rectY1,0,0));
         break;
     case CIRCLE:
+        circleX1=event->pos().x();
+        circleY1=event->pos().y();
+        circles.push_back({circleX1,circleY1,1});//r=0可能会有除0问题
         break;
     case POLYGON:
         break;
@@ -82,7 +90,6 @@ void MainWindow::mouseReleaseEvent(QMouseEvent *)
     switch(state)
     {
     case RECT:
-
         break;
     case CIRCLE:
         break;
@@ -106,6 +113,7 @@ void MainWindow::mouseMoveEvent(QMouseEvent *event)
         rects.back().setCoords(rectX1,rectY1,event->pos().x(),event->pos().y());
         break;
     case CIRCLE:
+        circles.back().r=sqrt((event->pos().x()-circleX1)*(event->pos().x()-circleX1)+(event->pos().y()-circleY1)*(event->pos().y()-circleY1));
         break;
     case POLYGON:
         break;
@@ -137,18 +145,22 @@ void MainWindow::DDALine(QPainter* painter, int x1, int y1, int x2, int y2)
     }
 }
 
-void MainWindow::BresenhamCircle(QPainter* painter, int x, int y, int r)
+void MainWindow::BresenhamCircle(QPainter* painter, int x, int y, int r)//有问题
 {
     int edgeX,edgeY,p;
 
-    edgeX=x;
-    edgeY=y+r;
+    edgeX=0;
+    edgeY=r;
     p=3-2*r;
     for(;edgeX<=edgeY;edgeX++)
     {
-        painter->drawPoint(edgeX,edgeY);//上
-        painter->drawPoint(2*x-edgeX,2*y-edgeY);//下
-
+        painter->drawPoint(x+edgeX,y+edgeY);//上
+        double dT=0;
+        for(int dTNC=1;dTNC<8;dTNC++)//画剩下7个点
+        {
+            dT=dTNC*0.25*Pi;
+            painter->drawPoint( x + r*cos(acos(edgeX/double(r))+dT), y + r*sin(asin(edgeY/double(r))+dT) );
+        }
         if(p>=0)
         {
             p+=4*(edgeX-edgeY)+10;
@@ -159,4 +171,48 @@ void MainWindow::BresenhamCircle(QPainter* painter, int x, int y, int r)
     }
 
 
+}
+
+void MainWindow::MidpointCircle2(QPainter *painter, double x0, double y0, double r)
+{
+    double x,y,dx,dy,d;
+    x=0;
+    y=r;
+    d=1-r;
+    dx=3;
+    dy=2-r-r;
+    painter->drawPoint(x0+x,y0+y);
+
+    double dT=0;
+    for(int dTNC=1;dTNC<8;dTNC++)//画剩下7个点
+    {
+        dT=dTNC*0.25*Pi;
+        painter->drawPoint( x0 + r*cos(acos(x/r)+dT), y0 + r*sin(asin(y/r)+dT) );
+    }
+
+    while(x<y)
+    {
+        if(d<0)
+        {
+            d+=dx;
+            dx+=2;
+            x++;
+        }
+        else
+        {
+            d+=(dx+dy);
+            dx+=2;
+            dy+=2;
+            x++;
+            y--;
+        }
+        painter->drawPoint(x0+x,y0+y);
+
+        for(int dTNC=1;dTNC<8;dTNC++)//画剩下7个点
+        {
+            dT=dTNC*0.25*Pi;
+            painter->drawPoint( x0 + r*cos(acos(x/r)+dT), y0 + r*sin(asin(y/r)+dT) );
+        }
+
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -6,6 +6,7 @@
 #include <QMouseEvent>
 #include <vector>
 #include <math.h>
+#define Pi 3.1415926535897932
 using namespace std;
 
 QT_BEGIN_NAMESPACE
@@ -13,6 +14,13 @@ namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
 enum State{NONE,RECT,CIRCLE,POLYGON,CUBE,BEZIER};
+
+struct Circle
+{
+    int x;
+    int y;
+    double r;
+};
 
 class MainWindow : public QMainWindow
 {
@@ -32,10 +40,14 @@ protected:
     void mouseMoveEvent(QMouseEvent*);
     void DDALine(QPainter* painter, int x1, int y1, int x2, int y2);
     void BresenhamCircle(QPainter* painter, int x, int y, int r);
+    void MidpointCircle2(QPainter* painter, double x0, double y0, double r);
     bool ifPaintMouseClickText=false;
     State state=NONE;
     vector<QRect> rects;
+    vector<Circle> circles;
     int rectX1;
     int rectY1;
+    int circleX1;
+    int circleY1;
 };
 #endif // MAINWINDOW_H
